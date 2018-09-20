144c64329100489131b37ef15e8404a050beebd7
hinhilla <hinhilla@list.ru>
2018-09-20 18:56:16 +0300

    лол

--- js/script.js
+++ js/script.js
@@ -13,26 +13,29 @@ var storage = "";
     isStorageSupport = false;
   };
 
-
-
 button.addEventListener("click", function (evt) {
 evt.preventDefault();
 popup.classList.toggle("form-hotel-show");
+popup.classList.remove("modal-error");
  if (storage) {
       dateIn.value = storage;
       dateOut.value = storage;
-    }});
+    }
+  });
 
 form.addEventListener("submit", function (evt) {
 if (!dateIn.value || !dateOut.value) {
 evt.preventDefault();
+popup.classList.remove("modal-error");
+popup.offsetWidth = popup.offsetWidth;
 popup.classList.add("modal-error");
+
     } else {
       if (isStorageSupport) {
       localStorage.setItem("date-in", dateIn.value);
       localStorage.setItem("date-out", dateOut.value);
     }
-}
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
